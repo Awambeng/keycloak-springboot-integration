af97921b61b3532d778de9c06b91bdb2885f0f6f
Awambeng Rodrick <awambengrodrick@gmail.com>
2024-10-26 10:52:35 +0100

    initial commit

---
+++ src/test/java/com/example/keycloak_springboot_integration/KeycloakSpringbootIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak_springboot_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringbootIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
